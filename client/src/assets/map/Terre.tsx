f0df4e9525fa324a1656c69be8feb3a13ec4da0b
RemiFELIN <43740654+RemiFELIN@users.noreply.github.com>
2018-11-01 23:37:21 +0100

    Add files via upload
    ajout de la map

---
+++ client/src/assets/map/Terre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Terre" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="tf_beach_tileset/RMVX/tf_beach_tileB.png" width="512" height="512"/>
+</tileset>
