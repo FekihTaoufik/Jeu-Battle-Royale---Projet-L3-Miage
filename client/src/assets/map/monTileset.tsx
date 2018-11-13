b0a1ba65ef4938ff0905ede0bcbd6b5d7a3ffc85
Fekih <tafekih@outlook.fr>
2018-11-13 09:18:15 +0100

    Utilisation d'un boilerplate vue js minimaliste

--- client/src/assets/map/monTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="monTileset" tilewidth="32" tileheight="32" tilecount="234" columns="18">
- <image source="tf_beach_tileset/beach_tileset.png" width="576" height="416"/>
-</tileset>
