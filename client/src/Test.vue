7bb3e39bff5680a015e44a99e78216d2498d896a
RemiFELIN <remi.felin@gmail.com>
2018-11-14 09:24:19 +0100

    test

---
+++ client/src/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+import Jeu from "./assets/js/jeu.js";
+export default {
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
