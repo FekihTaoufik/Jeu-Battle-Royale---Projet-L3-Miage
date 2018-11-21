2d757d1abf5345c3a978a05fe7755766ade02669
Fekih <tafekih@outlook.fr>
2018-11-21 10:54:41 +0100

    organisation joueurs socketio

--- server/sockets/player.js
+++ server/sockets/player.js
@@ -1,8 +1,25 @@
 const _ = require('lodash');
 const mo = require('moment');
 module.exports = (io) => {
+    var players=[];
+    var game ={
+        players:[]
+    }
     io.on('connect',function(client){
-        console.log("CLIENT CONNECTED");
+        players.push({
+            id : client.id,
+            pseudo: null
+        });
+        console.log(`➕  Joueur ${client.id} s'est connecté`)
+        console.log(`📢  Joueurs connecté : ${players.length} [${_.join(_.map(players,'id'),', ')}]`);
+        client.on('disconnect',()=>{
+            console.log(`➖  Joueur ${client.id} s'est déconnecté`)
+            players = players.filter(o=>{ return o.id != client.id })
+            console.log(`📢  Joueurs connecté : ${players.length} [${_.join(_.map(players,'id'),', ')}]`);
+        })
+        client.on('join_game',(player)=>{
+            
+        })
         client.on('player_moving',(player)=>{
             client.broadcast('player_moving',player);
         });
